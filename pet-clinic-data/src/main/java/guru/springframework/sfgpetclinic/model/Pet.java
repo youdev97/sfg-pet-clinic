439aacd2a9f4b90377cfdc806468cd575623c299
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-01-25 13:20:04 +0100

    added Pets and Contact info to owners on startup

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,28 +3,42 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
-	
+
 	private PetType petType;
 	private Owner owner;
 	private LocalDate birthDate;
-	
+	private String name;
+
 	public PetType getPetType() {
 		return petType;
 	}
+
 	public void setPetType(PetType petType) {
 		this.petType = petType;
 	}
+
 	public Owner getOwner() {
 		return owner;
 	}
+
 	public void setOwner(Owner owner) {
 		this.owner = owner;
 	}
+
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -5,10 +5,21 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
 
 	@Override
 	public Set<Owner> findAll() {
@@ -22,7 +33,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner save(Owner object) {
-		return super.save(object);
+		if (object != null) {
+			if(object.getPets() != null) {
+				object.getPets().forEach(pet -> {
+					if(pet.getPetType() != null) { //if null it means it's not saved in db
+						pet.setPetType(petTypeService.save(pet.getPetType()));
+					} else {
+						throw new RuntimeException("Pet Type is required");
+					}
+					
+					if(pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+			return super.save(object);
+		} else {
+			return null;
+		}
+
 	}
 
 	@Override
@@ -40,7 +70,4 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 		return null;
 	}
 
-		
-
-	
 }
