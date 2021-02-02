036a00e91296615b267635e6cc9110ed5f5ef1b3
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-02-02 17:05:37 +0100

    vets list specialties + Vets JSON End point

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Controller
@@ -16,10 +21,18 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html", "/vets.html" })
-	public String listVets(Model model) {
-		model.addAttribute("vets", vetService.findAll());
-		return "vets/index";
-	}
+	@RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
+        return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
+    }
 
 }
